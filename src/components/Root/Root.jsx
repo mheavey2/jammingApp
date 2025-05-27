b9495ef348b4b4b9851af6e2dc5141b650541de0
mheavey2 <mheavey2@gmail.com>
2025-05-27 13:10:29 +0100

    add logo and title and associated styling

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,32 @@
+import styles from "./root.module.css";
+import jamminLogo from "/headphonesLogo.png";
+import { Outlet } from "react-router-dom";
+
+export default function Root() {
+  return (
+    <>
+      <div className={styles.outerContainer}>
+        <header className={styles.loginHeader}>
+          <img
+            src={jamminLogo}
+            alt="Jammin Logo"
+            className={styles.jamminLoginLogo}
+          />
+          <h1>
+            <span className={styles.firstWave}>J</span>
+            <span className={styles.secondWave}>A</span>
+            <span className={styles.firstWave}>M</span>
+            <span className={styles.secondWave}>M</span>
+            <span className={styles.firstWave}>I</span>
+            <span className={styles.secondWave}>N</span>
+            <span className={styles.waveFirstLetter}> </span>
+            <span className={styles.firstWave}>A</span>
+            <span className={styles.secondWave}>P</span>
+            <span className={styles.firstWave}>P</span>
+          </h1>
+        </header>
+        <Outlet />
+      </div>
+    </>
+  );
+}
